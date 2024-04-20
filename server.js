ce3e9814852f6412dfd7cb323cf7defcf83000ae
longnguyen-03 <134860213+longnguyen-03@users.noreply.github.com>
2024-04-20 21:22:23 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -11,9 +11,28 @@ app.listen(port, () => {
 const uri='mongodb+srv://rongbang0304:03122004l@cluster0.ssbyfzt.mongodb.net/long1';
 const sv=require('./sv');
 const mongoose=require('mongoose');
+const svmodel = require('./sv');
 app.get('/', async(req, res)=>{
     await mongoose.connect(uri);
     let tests= await sv.find();
     console.log(tests);
     res.send(tests);
+})
+
+app.get('/xoa:id',  async(req, res) => {
+    await mongoose.connect(uri);
+    let id= req.params.id;
+    console.log(id);
+    await svmodel.deleteOne({_id:id});
+    res.redirect('../')
+})
+
+app.get('/update/:ten',async (req, res) => {
+    await mongoose.connect(uri);
+    let ma=req.params.ma;
+    console.log(ma);
+    let masvmoi=ma +'sv moi';
+    await svmodel.updateOne({ma: ma}, {ma: masvmoi});
+    let svn= await svmodel.find({});
+    res.send(svn);
 })
